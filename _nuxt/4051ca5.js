4390c36901baf5a48763cae76a4b6180aaee5cf3
胡浩 <huhaowangyi@163.com>
2021-07-29 10:39:24 +0800

    fix: 挪到根目录

---
+++ _nuxt/4051ca5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{514:function(n,t,e){"use strict";e.r(t);var c={components:{},data:function(){return{}}},o=e(95),component=Object(o.a)(c,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,"242b21c2",null);t.default=component.exports}}]);
