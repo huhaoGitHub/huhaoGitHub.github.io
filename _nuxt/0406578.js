ce3f4add942768887635f09b48357616f6a4fcec
胡浩 <huhaowangyi@163.com>
2021-07-29 10:22:51 +0800

    fix: 删除部分组件

--- _nuxt/0406578.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{530:function(n,t,e){"use strict";e.r(t);var c={components:{},data:function(){return{}}},o=e(92),component=Object(o.a)(c,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,"242b21c2",null);t.default=component.exports}}]);
